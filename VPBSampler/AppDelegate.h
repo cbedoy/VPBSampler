52feca6c7f7f9f27d220b1be19c83443cf16fc1b
Jesús Cagide <jesuscagide@Jesuss-MacBook-Air.local>
2015-07-15 22:20:11 -0700

    Initial Commit

---
+++ VPBSampler/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VPBSampler
+//
+//  Created by Jesus Cagide on 7/15/15.
+//  Copyright © 2015 Jesus Cagide. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VPBSampler/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VPBSampler
+//
+//  Created by Jesus Cagide on 7/15/15.
+//  Copyright © 2015 Jesus Cagide. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
